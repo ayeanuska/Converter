723d0a8f2d4e28eace8457d6e3a525869ff2b3c1
Anuska <anuskasth2022@gmail.com>
2024-12-13 22:19:59 +1100

    length converter trial

--- src/LengthConverter.jsx
+++ src/LengthConverter.jsx
@@ -18,27 +18,82 @@ const LengthConverter = () => {
 
   return (
     <>
-      <div className="container">
-        <h2 text-center fw-bold>
-          Length Converter
-        </h2>
+      <div className="lengthConverterWrapper max-w-screen-2xl bg-white rounded-lg p-3 m-5">
+        <label htmlFor="LengthMeasure" className="m-3">
+          Enter length
+        </label>
+        <input
+          type="number"
+          name="inputValue"
+          id="inputValue"
+          className="w-full p-2 rounded-lg border border-gray-300"
+          onChange={(e) => {
+            setInputvalue(e.target.value);
+          }}
+        />
+        <label htmlFor="lengthUnit" className=" m-3">
+          Unit
+        </label>
+        <select
+          name="units"
+          id="inputLengthUnit"
+          defaultValue="default"
+          // value={inputLengthUnit}
+          onChange={handleInputUnitChange}
+          className=" m-2 rounded-lg"
+        ></select>
 
-        <div className="row mt-5 card shadow-lg border-0 rounded">
-          <div className="col-md-5 card shadow-lg border rounded">
-            <div className="card-body p-4"></div>
-            <div className="mb-3">
-              <label htmlFor="meter" className="form-label fw-bold"></label>
-              <input
-                type="number"
-                id="celcius"
-                className="form-control border primary"
-                value={meter}
-                onChange={handleMeterChange}
-                placeholder="Enter in Meter"
-              />
-            </div>
-          </div>
+        <option disabled value="default">
+          select unit
+        </option>
+        <option value="km">Kilometer (km)</option>
+        <option value="m">Meter (m)</option>
+        <option value="cm">Centimeter (cm)</option>
+        <option value="mi">Miles (mi)</option>
+        <option value="ft">Feet (ft)</option>
+        <option value="in">Inches (in)</option>
+
+        <div>
+          <label htmlFor="outputLengthUnit" className=" m-3">
+            Convert To
+          </label>
+          <select
+            name="units"
+            id="outputLengthUnit"
+            defaultValue="default"
+            onChange={handleOutputUnitChange}
+            className=" rounded-lg"
+          >
+            <option disabled value="default">
+              select unit
+            </option>
+            <option value="km">Kilometer (km)</option>
+            <option value="m">Meter (m)</option>
+            <option value="cm">Centimeter (cm)</option>
+            <option value="mi">Miles (mi)</option>
+            <option value="ft">Feet (ft)</option>
+            <option value="in">Inches (in)</option>
+          </select>
         </div>
+        <button
+          type="button"
+          className="bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          onClick={handleConversion}
+        >
+          Convert
+        </button>
+
+        <label htmlFor="outputLengthMeasure" className="text-white m-3">
+          Converted length
+        </label>
+        <input
+          value={outputValue}
+          type="text"
+          name="outputValue"
+          id="outputValue"
+          disabled
+          className="w-full p-2 rounded-lg border border-gray-300"
+        />
       </div>
     </>
   );
